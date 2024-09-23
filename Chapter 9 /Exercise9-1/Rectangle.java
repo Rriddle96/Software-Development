b8b2fed0ffa470f31b3a4ab42be38e90b600f906
Rriddle96 <newvegasr.riddle@gmail.com>
2024-09-23 09:47:53 -0600

    Add files via upload

---
+++ Chapter 9 /Exercise9-1/Rectangle.java
@@ -0,0 +1,23 @@
+
+	class Rectangle {
+		
+		double width = 1;
+		double height = 1;
+		
+		Rectangle() {
+			
+		}
+		
+		Rectangle( double newWidth, double newHeight) {
+			width = newWidth;
+			height = newHeight;
+		}
+		
+		double getArea() {
+			return height * width;
+		}
+		
+		double getPerimeter() {
+			return 2 * (height + width);
+		}
+}
